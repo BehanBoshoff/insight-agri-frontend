47f72c786fe216624b8984307b803c401741b215
Behan Boshoff <behan.boshoff@jumo.world>
2022-10-08 16:38:24 +0200

    Position map

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -48,12 +48,12 @@ export class MapComponent  implements AfterViewInit {
           popupAnchor: [13, 0],
         });
   
-    const marker = L.marker([position.latitude, position.longitude], { icon }).bindPopup('Angular Leaflet');
+    const marker = L.marker([position.latitude, position.longitude], { icon }).bindPopup('Mouton Citrus');
     marker.addTo(this.map);
   }
 
   private loadMap(): void {
-    this.map = L.map('map', {zoomSnap: 0}).setView([0, 0], 1);
+    this.map = L.map('map').setView([0, 0], 1);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
